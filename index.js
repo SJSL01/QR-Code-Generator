09b9cb0c18080fcf802a3a5436303a4df8a09a08
Simran Jyot SIngh Lall <120094702+SJSL01@users.noreply.github.com>
2023-01-24 16:44:06 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const QR = require("qrcode")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express();
+
+app.use(express.json())
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/generateQR", async (req, res) => {
+
+    const QR_CODE = await QR.toDataURL(req.body.url)
+
+    res.json(QR_CODE)
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("server up at 3010");
+})
